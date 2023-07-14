35ebfe827ff868fdb613e6b01510ba8371e633d5
PatrickLisiecki <impatricklisiecki@gmail.com>
2023-07-14 17:45:24 -0400

    Created handler for /signup endpoint

---
+++ controllers/signUpController.js
@@ -0,0 +1,32 @@
+const { User } = require("../models");
+const bcrypt = require("bcryptjs");
+
+const signUpController = async (req, res) => {
+    // Hash the password from the request
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+    try {
+        // Create a new user with the hashed password
+        const newUser = await User.create({
+            name: req.body.name,
+            password: hashedPassword,
+        });
+
+        res.status(201).json({
+            message: "User created successfully",
+            name: newUser.name,
+        });
+    } catch (err) {
+        if (err.name === "SequelizeValidationError") {
+            return res
+                .status(422)
+                .json({ errors: err.errors.map((e) => e.message) });
+        }
+        res.status(500).json({
+            message: "Error occurred while creating user!",
+            error: err,
+        });
+    }
+};
+
+module.exports = signUpController;
