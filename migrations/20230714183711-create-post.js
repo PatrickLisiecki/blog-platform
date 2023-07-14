4638aa962b14f295c2c3a7b319b48c5f04c651eb
PatrickLisiecki <impatricklisiecki@gmail.com>
2023-07-14 18:36:34 -0400

    Added title to migration

--- migrations/20230714183711-create-post.js
+++ migrations/20230714183711-create-post.js
@@ -9,6 +9,10 @@ module.exports = {
                 primaryKey: true,
                 type: Sequelize.INTEGER,
             },
+            title: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
             content: {
                 type: Sequelize.TEXT,
                 allowNull: false,
